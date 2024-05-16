69096a4c432f02f4b2ef6e2204338d1f9d76cbdb
ravi.singh <ravi.singh@1rivet.com>
2024-05-16 12:28:04 +0530

    feat:resolve merge conflicts

--- src/models/jobApplication.model.ts
+++ src/models/jobApplication.model.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export interface IJobApplication {
   jobPostId: string;
   firstName: string;
@@ -13,19 +12,3 @@ interface IJobCV {
   name: string;
   url: string;
 }
-=======
-export interface IJobApplication {
-  jobPostId: string;
-  firstName: string;
-  lastName: string;
-  contactNumber: string;
-  email: string;
-  cv: IJobCV;
-}
-
-interface IJobCV {
-  id: string;
-  name: string;
-  url: string;
-}
->>>>>>> fc9bf1dcf1ba50a4ce2b361fbab7618ea7fd2e30
--- src/services/jobApplication.service.ts
+++ src/services/jobApplication.service.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Collection, Db } from "mongodb";
 import client from "../server/mongo.server";
 import { IJobApplication } from "../models/jobApplication.model";
@@ -12,18 +11,3 @@ export async function insertJobApplicationIntoDatabase(
   const application = await applications.insertOne(jobApplication);
   return application;
 }
-=======
-import { Collection, Db } from "mongodb";
-import client from "../server/mongo.server";
-import { IJobApplication } from "../models/jobApplication.model";
-
-const db: Db = client.db("JobBoard");
-const applications: Collection = db.collection("job-applications");
-
-export async function insertJobApplicationIntoDatabase(
-  jobApplication: IJobApplication
-) {
-  const application = await applications.insertOne(jobApplication);
-  return application;
-}
->>>>>>> fc9bf1dcf1ba50a4ce2b361fbab7618ea7fd2e30
